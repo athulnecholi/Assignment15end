450e38779e71791e183896ba6b3ceecaacf8d353
Athul N (INFOSYS LIMITED) <v-nathul@microsoft.com>
2025-04-10 13:08:23 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const planRoutes = require('./routes/planRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const planRoutes = require("./Routes/planRoutes");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use('/plans', planRoutes);
+app.use("/plans", planRoutes);
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
     app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
